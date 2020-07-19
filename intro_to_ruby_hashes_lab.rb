827cda3e17a031aa2ca70f9eef4b464507107dec
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-19 13:26:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -59,12 +59,11 @@ def monopoly_with_fourth_tier
         four_pieces_owned:200
       },
       names:{
-        reading_railroad:{},
-        pennsylvania_railroad:{},
-        b_and_o_railroad:{},
-        shortline_railroad:{}
+        reading_railroad:{mortgage_value:100},
+        pennsylvania_railroad:{mortgage_value:200},
+        b_and_o_railroad:{mortgage_value:400},
+        shortline_railroad:{mortgage_value:800}
       }
     }
   }
-base_hash[:railroads][:names][:reading_railroad]=base_hash[:mortgage_value]
 end
