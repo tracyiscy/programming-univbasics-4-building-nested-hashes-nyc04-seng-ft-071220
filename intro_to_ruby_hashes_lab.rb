f3bb5579d75722740e205338166a357e652c51c5
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-19 12:52:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -54,7 +54,7 @@ def monopoly_with_fourth_tier
       pieces:4,
       rent_in_dollars:{
         one_piece_owned:25,
-        two_pieces_owned:50
+        two_pieces_owned:50,
         three_pieces_owned:100,
         four_pieces_owned:200
       },
